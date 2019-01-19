7541c7f11b7692708f37949a8a895b5f8bfd2229
Nimda <39852038+onurrozkaan@users.noreply.github.com>
2019-01-19 15:04:38 +0300

    ascii header of app & colors

---
+++ assets/colors.py
@@ -0,0 +1,24 @@
+from colorama import init,Fore,Back,Style
+
+init(autoreset=True)
+
+fore_green = Fore.GREEN
+fore_white = Fore.WHITE
+fore_red = Fore.RED
+fore_blue = Fore.BLUE
+fore_yellow = Fore.YELLOW
+fore_purple = Fore.MAGENTA
+    
+back_green = Back.GREEN
+back_white = Back.WHITE
+back_red = Back.RED
+back_blue = Back.BLUE
+back_yellow = Fore.YELLOW
+back_purple = Fore.MAGENTA
+
+green = fore_green
+white = fore_white
+red = fore_red
+blue = fore_blue
+yellow = fore_yellow
+purple = fore_purple
