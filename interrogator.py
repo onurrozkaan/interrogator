82d63c618d4c3f1ad25aee83887109311d852a26
Nimda <39852038+onurrozkaan@users.noreply.github.com>
2019-01-30 21:30:08 +0300

    update

--- interrogator.py
+++ interrogator.py
@@ -14,7 +14,9 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='This is a cyber security tool created by Nimda')
-parser.add_argument('-f','--function', type=str, help="Avaible functions= 'catch.boss' 'catch.location' 'catch.igathering'")
+parser.add_argument('-f','--function', type=str, help="Avaible functions= 'catch.boss' 'catch.infoip' 'catch.igathering'")
+parser.add_argument('-v','--version', type=str, const="version_controll", nargs='?', help="Version information.")
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -24,8 +26,11 @@ banner_printer()
 if args.function == 'catch.boss':
     login_scanner()
 
-elif args.function == 'catch.location':
+elif args.function == 'catch.infoip':
     getOutput()
+
+elif args.version == "version_controll":
+    print("Interrogator version is currently > @0.0.10-beta")
 
 elif args.function == 'catch.igathering':
     sys.stdout.write(red + "Enter Address Website > ")
